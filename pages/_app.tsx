c290ab960abcac33a5821092d1194ac41413ff72
adrianonobrega <adrianonobrega26@gmail.com>
2022-12-10 08:39:49 -0300

    create component footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
-import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
-import { theme } from "../styles/theme"
+import  theme  from "../styles/theme"
 import Layout from '../components/Layout'
+import GlobalStyles from '../styles/global';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
           <Layout>
           <Component {...pageProps} />
           </Layout>
